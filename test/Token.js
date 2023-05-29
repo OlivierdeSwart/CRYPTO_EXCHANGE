e7d2c476c98d0aa24d033d127259bdfb4c8b5606
Olivier de Swart <olivierdeswart@gmail.com>
2023-05-29 08:48:10 +0200

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,12 +6,15 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-  let token
+  let token, accounts, deployer
 
   beforeEach(async () => {
     // Fetch token from Blockchain
     const Token = await ethers.getContractFactory('Token');
     token = await Token.deploy('Dapp University', 'DAPP', '1000000');
+  
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', () => {
@@ -34,6 +37,10 @@ describe('Token', () => {
   
     it('has correct totalSupply', async () => {
       expect(await token.totalSupply()).to.equal(tokens(totalSupply));
+    });
+
+    it('assigns total supply to deployer', async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(tokens(totalSupply));
     });
   })
 
