50414b67460bdc294e416723b44b9f4f56cbe1d2
Olivier de Swart <olivierdeswart@gmail.com>
2023-05-31 15:01:06 +0200

    9. Deposit Tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -78,9 +78,9 @@ contract Token {
 		public 
 		returns (bool success)
 	{
-		require(_value <= allowance[_from][msg.sender]);
-		require(_value <= balanceOf[_from]);
-
+		require(_value <= balanceOf[_from], 'insufficient balance');
+		require(_value <= allowance[_from][msg.sender], 'insufficient allowance');
+		
 		// update allowance
 		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
--- test/Exchange.js
+++ test/Exchange.js
@@ -11,11 +11,19 @@ describe('Exchange', () => {
   const feePercent = 10
 
   beforeEach(async () => {
+    const Exchange = await ethers.getContractFactory('Exchange') //pass in the name of smart contract to get byte code (deployment prep)
+    const Token = await ethers.getContractFactory('Token') //pass in the name of smart contract to get byte code (deployment prep)
+
+    token1 = await Token.deploy('Cutie Token <3', 'QT', '1000000')
+
     accounts = await ethers.getSigners()
     deployer = accounts[0]
     feeAccount = accounts[1]
+    user1 = accounts[2]
 
-    const Exchange = await ethers.getContractFactory('Exchange') //pass in the name of smart contract to get byte code (deployment prep)
+    let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100))
+    await transaction.wait()
+
     exchange = await Exchange.deploy(feeAccount.address, feePercent) //deploy the byte code
   })
 
@@ -27,6 +35,47 @@ describe('Exchange', () => {
 
     it('tracks the fee percent', async () => {
       expect(await exchange.feePercent()).to.equal(feePercent)
+    })
+  })
+
+  describe('Depositing tokens', () => {
+    let transaction, result
+    let amount = tokens(10)
+
+  describe('Success', async () => {
+    beforeEach(async () => {
+      //approve token
+      transaction = await token1.connect(user1).approve(exchange.address, amount)
+      result = await transaction.wait()
+      //deposit token
+      transaction = await exchange.connect(user1).depositToken(token1.address,amount)
+      result = await transaction.wait()
+
+    })
+    
+      it('tracks the token deposit', async () => {
+      expect(await token1.balanceOf(exchange.address)).to.be.equal(amount)
+      expect(await exchange.tokens(token1.address, user1.address)).to.be.equal(amount)
+      expect(await exchange.balanceOf(token1.address, user1.address)).to.be.equal(amount)
+      })
+    
+
+    it('emits a Deposit event', async () => {
+        const event = result.events[1]
+        expect(await event.event).to.equal('Deposit')
+
+        const args = event.args
+        expect(args.token).to.equal(token1.address) 
+        expect(args.user).to.equal(user1.address)
+        expect(args.amount).to.equal(amount)
+        expect(args.balance).to.equal(amount)
+      })
+    })
+
+    describe('Failure', () => {
+      it('fails when no tokens are approved', async () => {
+        await expect(exchange.connect(user1).depositToken(token1.address, amount)).to.be.reverted
+      })
     })
   })
 })
